a1e7233c012b217fbe139058dc8aee6712923d35
HossainNur <nurhossainsani1@gmail.com>
2024-07-31 12:12:49 +0600

    created: RegistrationActivity & layout file

---
+++ app/src/main/java/com/nurhossain/assignmentlifeplus/ui/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.nurhossain.assignmentlifeplus.ui.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nurhossain.assignmentlifeplus.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
