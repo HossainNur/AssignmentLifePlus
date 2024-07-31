dcb94e506266225f6f9de4f276759e0f65751f23
HossainNur <nurhossainsani1@gmail.com>
2024-07-31 12:13:43 +0600

    created: LoginActivity & layout file

---
+++ app/src/main/java/com/nurhossain/assignmentlifeplus/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nurhossain.assignmentlifeplus.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nurhossain.assignmentlifeplus.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
